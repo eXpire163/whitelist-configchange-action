2ddfb37c4167a7039c07b248d0a743bacb7301b4
eXpire163 <github@mms2k.de>
2021-11-19 12:31:15 +0100

    update

--- src/validation.ts
+++ src/validation.ts
@@ -44,14 +44,6 @@ export function getDiffOptions(){
             // default 60, minimum string length (left and right sides) to use text diff algorythm: google-diff-match-patch
             minLength: 60
         },
-        propertyFilter: function (name: string, context: any) {
-            /*
-             this optional function can be specified to ignore object properties (eg. volatile data)
-              name: property name, present in either context.left or context.right objects
-              context: the diff context (has context.left and context.right objects)
-            */
-            return name.slice(0, 1) !== '$';
-        },
         cloneDiffValues: false /* default false. if true, values in the obtained delta will be cloned
       (using jsondiffpatch.clone by default), to ensure delta keeps no references to left or right objects. this becomes useful if you're diffing and patching the same objects multiple times without serializing deltas.
       instead of true, a function can be specified here to provide a custom clone(value)
