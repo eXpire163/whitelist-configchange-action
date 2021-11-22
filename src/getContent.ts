3a619790827557f0309ff29d872234aa2d83ab56
eXpire163 <github@mms2k.de>
2021-11-22 14:54:14 +0100

    update

--- src/getContent.ts
+++ src/getContent.ts
@@ -1,16 +1,19 @@
 import { parse } from 'yaml';
 import { Buffer } from 'buffer';
+import { OctoType } from './types/OctoType';
+import * as core from '@actions/core';
 
 
 //retrieve the content of a file as plane text
-export async function getContent(contentRequest: any, octokit: any) {
-  const result = await octokit.rest.repos.getContent(contentRequest);
+export async function getContent(contentRequest: any, octokit: OctoType) {
+  const fileContent = await octokit.rest.repos.getContent(contentRequest);
   //core.info("oldFileResult: " + resultOld)
-  if (!result) {
-    //core.info("old result was empty")
+  if (fileContent?.data?.toString() === undefined ) {
+    core.error("file result was empty")
     return null;
   }
-  const content = Buffer.from(result.data.content, 'base64').toString();
+
+  const content = Buffer.from(fileContent.data.toString(), 'base64').toString();
   //core.info(contentRequest, contentOld)
   return parse(content);
 }
